a52085efaaafa37a98c353912f5fd67248341f4c
Kacper Pecka <s25668@pjwstk.edu.pl>
2023-03-14 19:43:41 +0100

    zadania

---
+++ zjazd1/zad10.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        <input type="number" name="a"><br>
+        <input type="submit">
+    </form>
+    <?php
+    if (isset($_POST['a'])) {
+        echo "<pre>";
+        $a = is_numeric($_POST['a']) ? $_POST['a'] : 1;
+        for ($i = 0; $i <= $a; $i++) {
+            echo str_repeat("*", $i) . "<br>";
+        }
+        for ($i = $a; $i > 0; $i--) {
+            echo str_repeat("*", $i) . "<br>";
+        }
+        for ($i = 0; $i < $a; $i++) {
+            echo str_repeat("&nbsp;", $i) . str_repeat("*", $a - $i) . "<br>";
+        }
+        for ($i = $a - 1; $i >= 0; $i--) {
+            echo str_repeat("&nbsp;", $i) . str_repeat("*", $a - $i) . "<br>";
+        }
+        echo "</pre>";
+    }
+    ?>
+</body>
+
+</html>
