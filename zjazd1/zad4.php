acdc346a8423ecb5ce097e78cc163c74ba217a08
Kacper Pecka <s25668@pjwstk.edu.pl>
2023-05-07 19:52:29 +0200

    zad 4 Exception if a || b = string

--- zjazd1/zad4.php
+++ zjazd1/zad4.php
@@ -22,21 +22,25 @@
     if (isset($_POST['a']) && isset($_POST['b'])) {
         $a = is_numeric($_POST['a']) ? $_POST['a'] : null;
         $b = is_numeric($_POST['b']) ? $_POST['b'] : null;
-        echo
-        "<br>" . $a . " + " . $b . " = " . ($a + $b) .
-            "<br>" . $a . " - " . $b . " = " . ($a - $b) .
-            "<br>" . $a . " * " . $b . " = " . ($a * $b) .
-            "<br>" . $a . " % " . $b . " = ";
-        try {
-            if (!$a || !$b) {
-                throw new Exception("<span style='color:red'>BŁĄD</span>");
+        if ($a == null || $b == null) {
+            echo
+            "<br>" . $a . " + " . $b . " = " . ($a + $b) .
+                "<br>" . $a . " - " . $b . " = " . ($a - $b) .
+                "<br>" . $a . " * " . $b . " = " . ($a * $b) .
+                "<br>" . $a . " % " . $b . " = ";
+            try {
+                if (!$a || !$b) {
+                    throw new Exception("<span style='color:red'>BŁĄD</span>");
+                }
+                if ($b == 0) {
+                    throw new Exception("<span style='color:red'>BŁĄD</span>");
+                }
+                echo $a % $b;
+            } catch (Exception $e) {
+                echo $e->getMessage();
             }
-            if ($b == 0) {
-                throw new Exception("<span style='color:red'>BŁĄD</span>");
-            }
-            echo $a % $b;
-        } catch (Exception $e) {
-            echo $e->getMessage();
+        } else {
+            echo "BŁĄD";
         }
     }
     ?>
